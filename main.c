dab2078311001b550bbf0f3ef7fca2357f09d2db
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-07-28 12:11:28 +0200

    Rename a variable.

--- main.c
+++ main.c
@@ -24,45 +24,44 @@ l:  free(res1);
 
 void test_0(void)
 {
-    char orig_str[] = "People of Earth";
+    char str[] = "People of Earth";
 
     printf("Test 0: No bug on the execution path\n");
-    gen_test(orig_str, 3);
+    gen_test(str, 3);
 }
 
 void test_1(void)
 {
-    char orig_str[] = "People of Earth, your attention please";
+    char str[] = "People of Earth, your attention please";
 
     printf("Test 1: Shift with a negative input\n");
-    gen_test(orig_str, -3);
+    gen_test(str, -3);
 }
 
 void test_2(void)
 {
-    char orig_str[] = "People of Earth, your attention please";
+    char str[] = "People of Earth, your attention please";
 
     printf("Test 2: Shift with a positive input\n");
-    gen_test(orig_str, 7);
+    gen_test(str, 7);
 }
 
 void test_3(void)
 {
-    char orig_str[] = "People of Earth, your attention please";
+    char str[] = "People of Earth, your attention please";
 
     printf("Test 3: Shift with INT_MIN\n");
-    gen_test(orig_str, INT_MIN);
+    gen_test(str, INT_MIN);
 }
-
 
 void main(void)
 {
-    char orig_str[] = "People of Earth, your attention please";
+    char str[] = "People of Earth, your attention please";
 
     printf("Test 1: Shift with a negative input\n");
-    gen_test(orig_str, -3);
+    gen_test(str, -3);
     printf("\nTest 2: Shift with a positive input\n");
-    gen_test(orig_str, 7);
+    gen_test(str, 7);
     printf("\nTest 3: Shift with INT_MIN\n");
-    gen_test(orig_str, INT_MIN);
+    gen_test(str, INT_MIN);
 }
