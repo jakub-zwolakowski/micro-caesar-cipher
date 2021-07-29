6f42ffd7ce9203b7b567cc183a0defe4e8a157b3
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-07-29 11:17:28 +0200

    Remove null pointer checks.

--- caesar.c
+++ caesar.c
@@ -18,20 +18,18 @@ char *caesar_encrypt(char *str, int shift)
     int i = 0;
     char *buf = (char *)malloc(33);
 
-    if (buf) {
-        while (*str) {
-            if (*str >= 'a' && *str <= 'z')
-                buf[i] = (*str + abs_shift - 'a') % NB_LTR + 'a';
-            else if (*str >= 'A' && *str <= 'Z')
-                buf[i] = (*str + abs_shift - 'A') % NB_LTR + 'A';
-            else
-                /* Spaces and other characters are not encrypted. */
-                buf[i] = *str;
-            i++;
-            str++;
-        }
-        buf[i] = '\0';
+    while (*str) {
+        if (*str >= 'a' && *str <= 'z')
+            buf[i] = (*str + abs_shift - 'a') % NB_LTR + 'a';
+        else if (*str >= 'A' && *str <= 'Z')
+            buf[i] = (*str + abs_shift - 'A') % NB_LTR + 'A';
+        else
+            /* Spaces and other characters are not encrypted. */
+            buf[i] = *str;
+        i++;
+        str++;
     }
+    buf[i] = '\0';
 
     return buf;
 }
@@ -42,20 +40,18 @@ char *caesar_decrypt(char *str, int shift)
     int i = 0;
     char *buf = (char *)malloc(33);
 
-    if (buf) {
-        while (*str) {
-            if (*str >= 'a' && *str <= 'z')
-                buf[i] = (*str + (NB_LTR - abs_shift) - 'a') % NB_LTR + 'a';
-            else if (*str >= 'A' && *str <= 'Z')
-                buf[i] = (*str + (NB_LTR - abs_shift) - 'A') % NB_LTR + 'A';
-            else
-                /* Spaces and other characters are not encrypted. */
-                buf[i] = *str;
-            i++;
-            str++;
-        }
-        buf[i] = '\0';
+    while (*str) {
+        if (*str >= 'a' && *str <= 'z')
+            buf[i] = (*str + (NB_LTR - abs_shift) - 'a') % NB_LTR + 'a';
+        else if (*str >= 'A' && *str <= 'Z')
+            buf[i] = (*str + (NB_LTR - abs_shift) - 'A') % NB_LTR + 'A';
+        else
+            /* Spaces and other characters are not encrypted. */
+            buf[i] = *str;
+        i++;
+        str++;
     }
+    buf[i] = '\0';
 
     return buf;
 }
